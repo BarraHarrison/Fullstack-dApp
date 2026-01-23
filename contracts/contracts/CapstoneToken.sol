0e093324df7fefc5d9e44a05b9e8562e35e690f3
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2026-01-23 10:45:16 +0900

    Skeleton Solidity contract

---
+++ contracts/contracts/CapstoneToken.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title CapstoneToken
+ * @author Barra Harrison
+ * @notice ERC-20 token used in the Week 12 fullstack dApp capstone
+ *
+ * This contract is intentionally simple and audit-friendly.
+ * It will be interacted with by:
+ *  - a React frontend
+ *  - a Node/Express backend
+ *
+ * Backend services will index events emitted from this contract.
+ */
+contract CapstoneToken is ERC20, Ownable {
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        uint256 initialSupply
+    ) ERC20(name_, symbol_) {
+        _mint(msg.sender, initialSupply);
+    }
+}
