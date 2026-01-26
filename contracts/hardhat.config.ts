18ae0deb2e30dbaea6a2c0e6a41165711dbe63a8
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2026-01-26 10:32:39 +0900

    Sepolia network and dotenv added

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,6 +1,9 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@typechain/hardhat";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -11,6 +14,15 @@ const config: HardhatUserConfig = {
         runs: 200,
       },
     },
+  },
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY || "",
   },
   typechain: {
     outDir: "typechain-types",
