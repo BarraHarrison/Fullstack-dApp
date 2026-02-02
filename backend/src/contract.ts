b9d2d5353f83d9372d38f667c978a6bc17e0b80d
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2026-02-02 21:15:17 +0900

    Contract deployed at new address on the hardhat local blockchain

--- backend/src/contract.ts
+++ backend/src/contract.ts
@@ -3,7 +3,7 @@ import { provider } from "./provider";
 import CapstoneTokenArtifact from "./abi/CapstoneToken.json";
 
 export const CONTRACT_ADDRESS =
-    "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+    "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
 
 const abi = CapstoneTokenArtifact.abi;
 
